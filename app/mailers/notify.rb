8f627ac74acb151bd4dfd4efc356e87f82cb59a7
Pierre de La Morinerie <pierre@capitainetrain.com>
2014-02-21 19:39:09 +0100

    Send notification emails to the "project", and put people in Cc
    This fixes email threading in Mail.app, that doesn't like when a thread
    doesn't have stable recipients.
    
    For instance, here is a possible sender-recipient combinations before:
    
    From: A
    To: Me
    New issue
    
    From: B
    To: Me
    Reply on new issue
    
    From: A
    To: Me
    Another reply
    
    Mail.app doesn't see B as a participant to the original email thread,
    and decides to break the thread: it will group all messages from A
    together, and separately all messages from B.
    
    This commit makes the thread look like this:
    
    From: A
    To: gitlab/project
    Cc: Me
    New issue
    
    From: B
    To: gitlab/project
    Cc: Me
    Reply on new issue
    
    From: A
    To: gitlab/project
    Cc: Me
    Another reply
    
    Mail.app sees a common recipient, and group the thread correctly.

--- app/mailers/notify.rb
+++ app/mailers/notify.rb
@@ -19,6 +19,7 @@ class Notify < ActionMailer::Base
   default_url_options[:script_name] = Gitlab.config.gitlab.relative_url_root
 
   default from: Proc.new { default_sender_address.format }
+  default to:   Proc.new { project_sender_address.format }
   default reply_to: "noreply@#{Gitlab.config.gitlab.host}"
 
   # Just send email with 2 seconds delay
@@ -41,6 +42,17 @@ class Notify < ActionMailer::Base
     address = Mail::Address.new(Gitlab.config.gitlab.email_from)
     address.display_name = "GitLab"
     address
+  end
+
+  # The default email address to send emails to. Includes the project name if possible.
+  def project_sender_address
+    if @project
+      address = default_sender_address
+      address.display_name = @project.name_with_namespace
+      address
+    else
+      default_sender_address
+    end
   end
 
   # Return an email address that displays the name of the sender.
@@ -110,6 +122,9 @@ class Notify < ActionMailer::Base
   #
   # See: mail_answer_thread
   def mail_new_thread(model, headers = {}, &block)
+    raise ArgumentError, '"To:" header will be overwritten; use "Cc:" or "Bcc:"' unless headers[:to].nil?
+    headers[:to] = project_sender_address.format
+
     headers['Message-ID'] = message_id(model)
     mail(headers, &block)
   end
@@ -121,8 +136,12 @@ class Notify < ActionMailer::Base
   #
   #  * have a subject that begin by 'Re: '
   #  * have a 'In-Reply-To' or 'References' header that references the original 'Message-ID'
+  #  * have stable 'From' and 'To' headers between messages of the same thread
   #
   def mail_answer_thread(model, headers = {}, &block)
+    raise ArgumentError, '"To:" header will be overwritten; use "Cc:" or "Bcc:"' unless headers[:to].nil?
+    headers[:to] = project_sender_address.format
+
     headers['In-Reply-To'] = message_id(model)
     headers['References'] = message_id(model)
 
