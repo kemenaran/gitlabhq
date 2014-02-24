9b001167bb276d1a38d286bf1a45af030421d250
Pierre de La Morinerie <pierre@capitainetrain.com>
2014-02-24 12:12:29 +0100

    Allow email clients to group email notifications by thread
    Send the correct headers to tell email clients to consider all emails
    related to the same model object as a single conversation thread.

--- app/mailers/emails/issues.rb
+++ app/mailers/emails/issues.rb
@@ -4,9 +4,10 @@ module Emails
       @issue = Issue.find(issue_id)
       @project = @issue.project
       @target_url = project_issue_url(@project, @issue)
-      mail(from: sender(@issue.author_id),
-           cc: recipient(recipient_id),
-           subject: subject("#{@issue.title} (##{@issue.iid})"))
+      mail_new_thread(@issue,
+                      from: sender(@issue.author_id),
+                      cc: recipient(recipient_id),
+                      subject: subject("#{@issue.title} (##{@issue.iid})"))
     end
 
     def reassigned_issue_email(recipient_id, issue_id, previous_assignee_id)
@@ -14,8 +15,9 @@ module Emails
       @previous_assignee = User.find_by(id: previous_assignee_id) if previous_assignee_id
       @project = @issue.project
       @target_url = project_issue_url(@project, @issue)
-      mail(cc: recipient(recipient_id),
-           subject: subject("#{@issue.title} (##{@issue.iid})"))
+      mail_answer_thread(@issue,
+                         cc: recipient(recipient_id),
+                         subject: subject("#{@issue.title} (##{@issue.iid})"))
     end
 
     def closed_issue_email(recipient_id, issue_id, updated_by_user_id)
@@ -23,9 +25,10 @@ module Emails
       @project = @issue.project
       @updated_by = User.find updated_by_user_id
       @target_url = project_issue_url(@project, @issue)
-      mail(from: sender(updated_by_user_id),
-           cc: recipient(recipient_id),
-           subject: subject("#{@issue.title} (##{@issue.iid})"))
+      mail_answer_thread(@issue,
+                         from: sender(updated_by_user_id),
+                         cc: recipient(recipient_id),
+                         subject: subject("#{@issue.title} (##{@issue.iid})"))
     end
 
     def issue_status_changed_email(recipient_id, issue_id, status, updated_by_user_id)
@@ -34,9 +37,10 @@ module Emails
       @project = @issue.project
       @updated_by = User.find updated_by_user_id
       @target_url = project_issue_url(@project, @issue)
-      mail(from: sender(updated_by_user_id),
-           cc: recipient(recipient_id),
-           subject: subject("#{@issue.title} (##{@issue.iid})"))
+      mail_answer_thread(@issue,
+                         from: sender(updated_by_user_id),
+                         cc: recipient(recipient_id),
+                         subject: subject("#{@issue.title} (##{@issue.iid})"))
     end
   end
 end
--- app/mailers/emails/merge_requests.rb
+++ app/mailers/emails/merge_requests.rb
@@ -4,9 +4,10 @@ module Emails
       @merge_request = MergeRequest.find(merge_request_id)
       @project = @merge_request.project
       @target_url = project_merge_request_url(@project, @merge_request)
-      mail(from: sender(@merge_request.author_id),
-           cc: recipient(recipient_id),
-           subject: subject("#{@merge_request.title} (!#{@merge_request.iid})"))
+      mail_new_thread(@merge_request,
+                      from: sender(@merge_request.author_id),
+                      cc: recipient(recipient_id),
+                      subject: subject("#{@merge_request.title} (!#{@merge_request.iid})"))
     end
 
     def reassigned_merge_request_email(recipient_id, merge_request_id, previous_assignee_id)
@@ -14,9 +15,10 @@ module Emails
       @previous_assignee = User.find_by(id: previous_assignee_id) if previous_assignee_id
       @project = @merge_request.project
       @target_url = project_merge_request_url(@project, @merge_request)
-      mail(from: sender(@merge_request.author_id),
-           cc: recipient(recipient_id),
-           subject: subject("#{@merge_request.title} (!#{@merge_request.iid})"))
+      mail_answer_thread(@merge_request,
+                         from: sender(@merge_request.author_id),
+                         cc: recipient(recipient_id),
+                         subject: subject("#{@merge_request.title} (!#{@merge_request.iid})"))
     end
 
     def closed_merge_request_email(recipient_id, merge_request_id, updated_by_user_id)
@@ -24,18 +26,20 @@ module Emails
       @updated_by = User.find updated_by_user_id
       @project = @merge_request.project
       @target_url = project_merge_request_url(@project, @merge_request)
-      mail(from: sender(@merge_request.author_id),
-           cc: recipient(recipient_id),
-           subject: subject("#{@merge_request.title} (!#{@merge_request.iid})"))
+      mail_answer_thread(@merge_request,
+                         from: sender(@merge_request.author_id),
+                         cc: recipient(recipient_id),
+                         subject: subject("#{@merge_request.title} (!#{@merge_request.iid})"))
     end
 
     def merged_merge_request_email(recipient_id, merge_request_id)
       @merge_request = MergeRequest.find(merge_request_id)
       @project = @merge_request.project
       @target_url = project_merge_request_url(@project, @merge_request)
-      mail(from: sender(@merge_request.author_id),
-           cc: recipient(recipient_id),
-           subject: subject("#{@merge_request.title} (!#{@merge_request.iid})"))
+      mail_answer_thread(@merge_request,
+                         from: sender(@merge_request.author_id),
+                         cc: recipient(recipient_id),
+                         subject: subject("#{@merge_request.title} (!#{@merge_request.iid})"))
     end
   end
 
