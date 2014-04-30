bc854aebde19505828266cc545c8ca41d630bdee
Pierre de La Morinerie <pierre@capitainetrain.com>
2014-04-30 10:11:09 +0200

    Don’t send the merge notification email to the merger

--- spec/services/notification_service_spec.rb
+++ spec/services/notification_service_spec.rb
@@ -264,19 +264,21 @@ describe NotificationService do
 
     describe :merged_merge_request do
       it do
+        should_email(merge_request.author_id)
         should_email(merge_request.assignee_id)
         should_email(@u_watcher.id)
+        should_not_email(@u_committer.id)
         should_not_email(@u_participating.id)
         should_not_email(@u_disabled.id)
-        notification.merge_mr(merge_request, @u_disabled)
+        notification.merge_mr(merge_request, @u_committer)
       end
 
       def should_email(user_id)
-        Notify.should_receive(:merged_merge_request_email).with(user_id, merge_request.id, @u_disabled.id)
+        Notify.should_receive(:merged_merge_request_email).with(user_id, merge_request.id, @u_committer.id)
       end
 
       def should_not_email(user_id)
-        Notify.should_not_receive(:merged_merge_request_email).with(user_id, merge_request.id, @u_disabled.id)
+        Notify.should_not_receive(:merged_merge_request_email).with(user_id, merge_request.id, @u_committer.id)
       end
     end
   end
